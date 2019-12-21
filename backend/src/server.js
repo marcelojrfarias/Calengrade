333fc4a5fe439bc5d3d41fabff989f78be62c002
marcelojrfarias <marcelojrfarias@outlook.com>
2019-12-21 13:03:49 -0300

    Add morgan

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const cors = require('cors');
 
+const morgan = require('morgan');
+
 const routes = require('./routes')
 
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use(morgan('dev'))
 
 app.use(routes);
 
