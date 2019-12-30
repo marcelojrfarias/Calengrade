878f96c722c5d7b34113bfc11c577c98685e397f
marcelojrfarias <marcelojrfarias@outlook.com>
2019-12-30 02:16:46 -0300

    And there we go... again and again

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,10 +21,10 @@ app.use(morgan('dev'))
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, '..', 'frontend', 'build')))
+    app.use(express.static('../../frontend/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '..', '..', 'frontend', 'build', 'index.html'))
     }) 
 }
 
