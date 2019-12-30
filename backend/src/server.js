b27e54397bc64420523603e083030d0b4134b737
marcelojrfarias <marcelojrfarias@outlook.com>
2019-12-30 02:07:39 -0300

    And there we go...

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,33 @@
 require('dotenv').config()
 
-const express = require('express');
+const express = require('express')
 
-const cors = require('cors');
+const cors = require('cors')
 
-const morgan = require('morgan');
+const morgan = require('morgan')
+
+const path = require('path')
 
 const routes = require('./routes')
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(morgan('dev'))
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => console.log(`Server ready on PORT: ${process.env.PORT || 3333}`));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '..', 'frontend', 'build')))
+
+    app.get('*', (res, req) => {
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
+    }) 
+}
+
+app.listen(process.env.PORT || 3333, () => {
+    console.log(`Server ready on PORT: ${process.env.PORT || 3333}`)
+});
