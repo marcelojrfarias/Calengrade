47e204cb485e29fe54e5f4060b46ba6fa068f6da
marcelojrfarias <marcelojrfarias@outlook.com>
2019-12-30 02:11:25 -0300

    And there we go... again

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,7 @@ app.use(routes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, '..', 'frontend', 'build')))
 
-    app.get('*', (res, req) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
     }) 
 }
