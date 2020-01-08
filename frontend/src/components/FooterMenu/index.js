3c0bec227cf53b3c892444e918b6c9aa40fbf3ee
marcelojrfarias <marcelojrfarias@outlook.com>
2020-01-08 19:49:04 -0300

    Add the Component syntax to FooterMenu

--- frontend/src/components/FooterMenu/index.js
+++ frontend/src/components/FooterMenu/index.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './styles.css'
+import { render } from '@testing-library/react'
 
-export default function FooterMenu(props) {
+class FooterMenu extends Component {
 
-  const { menuItems } = props;
-
-  return (
-    <div className="container">
-      {menuItems.map((menuItem, index) => (
-        <div key={index}>
-          <span>
-            <a rel="noopener noreferrer" target="_blank" href={menuItem.link}>{menuItem.title}</a>
-          </span>
-        </div>
-      ))}
-    </div>
-  )
+  render() {
+    const { menuItems } = this.props
+    return (
+      <div className="container">
+        {menuItems.map((menuItem, index) => (
+          <div key={index}>
+            <span>
+              <a rel="noopener noreferrer" target="_blank" href={menuItem.link}>{menuItem.title}</a>
+            </span>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
+
+export default FooterMenu
